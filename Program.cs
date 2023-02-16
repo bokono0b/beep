d785874699cd8b7153ab491ae48fc5719abe3723
dakata1337 <poshtazaprezentaciaa@gmail.com>
2023-02-16 15:59:55 +0200

    refactored

--- Program.cs
+++ Program.cs
@@ -1,19 +1,55 @@
-﻿ int[] DO = new int[] { 131, 262, 523, 1046 };
- int[] RE = new int[] { 147, 294, 587, 1174 };
- int[] MI = new int[] { 165, 330, 659, 1318 };
- int[] FA = new int[] { 175, 349, 698, 1396 };
- int[] SO = new int[] { 196, 392, 784, 1568 };
- int[] LA = new int[] { 220, 440, 880, 1760 };
- int[] TI = new int[] { 247, 494, 988, 1976 };
-
-void sweetChild()
+﻿class Program
 {
-    int oct1 = 0;
-    int oct2 = 1;
-    int oct3 = 2;
-    int oct4 = 3;
-    for (int i = 0; i < 2; i++)
+    static int[] DO = new int[] { 131, 262, 523, 1046 };
+    static int[] RE = new int[] { 147, 294, 587, 1174 };
+    static int[] MI = new int[] { 165, 330, 659, 1318 };
+    static int[] FA = new int[] { 175, 349, 698, 1396 };
+    static int[] SO = new int[] { 196, 392, 784, 1568 };
+    static int[] LA = new int[] { 220, 440, 880, 1760 };
+    static int[] TI = new int[] { 247, 494, 988, 1976 };
+
+    static void Main(string[] args) {
+        sweetChild();
+    }
+
+    static void sweetChild()
     {
+        int oct2 = 1;
+        int oct3 = 2;
+        int oct4 = 3;
+        for (int i = 0; i < 2; i++) {
+            Console.Beep(SO[oct2], 250);
+            Console.Beep(SO[oct3], 250);
+            Console.Beep(RE[oct3], 250);
+            Console.Beep(DO[oct3], 250);
+            Console.Beep(DO[oct4], 250);
+            Console.Beep(RE[oct3], 250);
+            Console.Beep(TI[oct3], 250);
+            Console.Beep(RE[oct3], 250);
+        }
+
+        for (int i = 0; i < 2; i++) {
+            Console.Beep(LA[oct2], 250);
+            Console.Beep(SO[oct3], 250);
+            Console.Beep(RE[oct3], 250);
+            Console.Beep(DO[oct3], 250);
+            Console.Beep(DO[oct4], 250);
+            Console.Beep(RE[oct3], 250);
+            Console.Beep(TI[oct3], 250);
+            Console.Beep(RE[oct3], 250);
+        }
+
+        for (int i = 0; i < 2; i++) {
+            Console.Beep(DO[oct3], 250);
+            Console.Beep(SO[oct3], 250);
+            Console.Beep(RE[oct3], 250);
+            Console.Beep(DO[oct3], 250);
+            Console.Beep(DO[oct4], 250);
+            Console.Beep(RE[oct3], 250);
+            Console.Beep(TI[oct3], 250);
+            Console.Beep(RE[oct3], 250);
+        }
+
         Console.Beep(SO[oct2], 250);
         Console.Beep(SO[oct3], 250);
         Console.Beep(RE[oct3], 250);
@@ -23,41 +59,5 @@ void sweetChild()
         Console.Beep(TI[oct3], 250);
         Console.Beep(RE[oct3], 250);
     }
-
-
-    for (int i = 0; i < 2; i++)
-    {
-        Console.Beep(LA[oct2], 250);
-        Console.Beep(SO[oct3], 250);
-        Console.Beep(RE[oct3], 250);
-        Console.Beep(DO[oct3], 250);
-        Console.Beep(DO[oct4], 250);
-        Console.Beep(RE[oct3], 250);
-        Console.Beep(TI[oct3], 250);
-        Console.Beep(RE[oct3], 250);
-    }
-
-    for (int i = 0; i < 2; i++)
-    {
-        Console.Beep(DO[oct3], 250);
-        Console.Beep(SO[oct3], 250);
-        Console.Beep(RE[oct3], 250);
-        Console.Beep(DO[oct3], 250);
-        Console.Beep(DO[oct4], 250);
-        Console.Beep(RE[oct3], 250);
-        Console.Beep(TI[oct3], 250);
-        Console.Beep(RE[oct3], 250);
-    }
-
-    Console.Beep(SO[oct2], 250);
-    Console.Beep(SO[oct3], 250);
-    Console.Beep(RE[oct3], 250);
-    Console.Beep(DO[oct3], 250);
-    Console.Beep(DO[oct4], 250);
-    Console.Beep(RE[oct3], 250);
-    Console.Beep(TI[oct3], 250);
-    Console.Beep(RE[oct3], 250);
 }
 
-
-sweetChild();
